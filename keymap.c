b5ccf372108e789ce4cf9e7068fb9cc3705acf77
Patrick Tsai <patrick@simplypatrick.com>
2023-08-21 23:09:34 -0700

    feat(planck): Remove Fn with LOWER and make easy ()[]

--- keymap.c
+++ keymap.c
@@ -361,17 +361,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Bksp |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   -  |   +  |   {  |   }  |      |
+ * | Del  |      |      |      |      |   (  |   )  |   -  |   +  |   {  |   }  |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |   =  |   <  |   >  |   \  |      |
+ * |      |      |      |      |      |   [  |   ]  |   =  |   <  |   >  |   \  |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_planck_grid(
     KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_BSPC,
-    KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS,    KC_PLUS,    KC_LCBR, KC_RCBR, TR_SCLN,
-    OS_LSFT, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_EQL,     KC_LABK,    KC_RABK, KC_BSLS, OS_RSFT,
+    KC_DEL,  _______, _______, _______, _______, KC_LPRN, KC_RPRN, KC_MINS,    KC_PLUS,    KC_LCBR, KC_RCBR, TR_SCLN,
+    OS_LSFT, _______, _______, _______, _______, KC_LBRC, KC_RBRC, KC_EQL,     KC_LABK,    KC_RABK, KC_BSLS, OS_RSFT,
     _______, _______, _______, _______, _______, TR_RARR, TR_RARR, _______,    KC_HOME,    KC_PGDN, KC_PGUP, KC_END
 ),
 
