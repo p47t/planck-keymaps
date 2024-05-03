9af02d119fc462ea76e45a02617584f2bd4b7de5
Patrick Tsai <patrick@simplypatrick.com>
2024-05-03 02:49:56 -0700

    feat(planck): Update key maps

--- keymap.c
+++ keymap.c
@@ -334,8 +334,8 @@ enum planck_layers {
   _PLOVER,
 };
 
-#define LOWER MO(_LOWER)
-#define RAISE MO(_RAISE)
+#define LOWER LT(_LOWER, KC_ESC)
+#define RAISE LT(_RAISE, KC_EQL)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -359,25 +359,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Lower
  * ,-----------------------------------------------------------------------------------.
- * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Bksp |
+ * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Del  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Del  |      |      |      |      |   (  |   )  |   -  |   +  |   {  |   }  |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |   [  |   ]  |   =  |   <  |   >  |   \  |      |
+ * |      |Cmd-Z |Cmd-X |Cmd-C |Cmd-V |   [  |   ]  |   =  |   <  |   >  |   \  |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
+ * |      |      |      |      |      |             |      | Home | Pg Dn| Pg Up| End  |
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_planck_grid(
-    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_BSPC,
-    KC_DEL,  _______, _______, _______, _______, KC_LPRN, KC_RPRN, KC_MINS,    KC_PLUS,    KC_LCBR, KC_RCBR, TR_SCLN,
-    OS_LSFT, _______, _______, _______, _______, KC_LBRC, KC_RBRC, KC_EQL,     KC_LABK,    KC_RABK, KC_BSLS, OS_RSFT,
+    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_DEL,
+    HY_TAB,  _______, _______, _______, _______, KC_LPRN, KC_RPRN, KC_MINS,    KC_PLUS,    KC_LCBR, KC_RCBR, TR_SCLN,
+    OS_LSFT, CMD_Z,   CMD_X,   CMD_C,   CMD_V,   KC_LBRC, KC_RBRC, KC_EQL,     KC_LABK,    KC_RABK, KC_BSLS, OS_RSFT,
     _______, _______, _______, _______, _______, TR_RARR, TR_RARR, _______,    KC_HOME,    KC_PGDN, KC_PGUP, KC_END
 ),
 
 /* Raise
  * ,-----------------------------------------------------------------------------------.
- * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
+ * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Del  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  | Down |  Up  |   [  |   ]  | Pg Up|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -387,7 +387,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_planck_grid(
-    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
     KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_DOWN, KC_UP,   KC_LBRC, KC_RBRC, KC_PGUP,
     KC_LSFT, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_HOME, KC_END,  KC_PIPE, KC_PGDN,
     _______, _______, _______, _______, _______, KC_UNDS, KC_UNDS, _______, KC_MUTE, KC_VOLD, KC_VOLU, KC_MPLY
