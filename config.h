a87ae4ad2f3899ce651bfae73d78ef4fc8d00ceb
Patrick Tsai <patrick@simplypatrick.com>
2024-05-03 01:49:02 -0700

    fix(planck): Compile errors with latest QMK firmware

--- config.h
+++ config.h
@@ -50,9 +50,6 @@
 
 #define RGBLIGHT_ANIMATIONS
 
-// Prevent normal rollover on alphas from accidentally triggering mods.
-#define IGNORE_MOD_TAP_INTERRUPT
-
 #define TAPPING_TERM 250
 
 #define LEADER_PER_KEY_TIMING
